64b7ff9ad1f3782557eed87f548e5a0b0090c11b
Virgilio Sabado Matendjua <matendjua91@gmail.com>
2021-02-08 08:50:11 +0200

    configuracao do propaties

--- src/main/java/com/sigespapi/sigespapi/controller/ReinoCotroller.java
+++ src/main/java/com/sigespapi/sigespapi/controller/ReinoCotroller.java
@@ -1,8 +1,12 @@
 package com.sigespapi.sigespapi.controller;
 
+
+
+import com.sigespapi.sigespapi.model.Reino;
 import com.sigespapi.sigespapi.repository.ReinoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +17,12 @@ public class ReinoCotroller {
     @Autowired
     ReinoRepository reinoDao;
 
-
+    @PostMapping("/save")
+    public String form(Reino reino){
+        reinoDao.save(reino);
+        return "redirect:/reino";
+    }
+            
+  
     
 }
--- src/main/java/com/sigespapi/sigespapi/repository/ReinoRepository.java
+++ src/main/java/com/sigespapi/sigespapi/repository/ReinoRepository.java
@@ -1,11 +1,13 @@
 package com.sigespapi.sigespapi.repository;
 
+
 import com.sigespapi.sigespapi.model.Reino;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReinoRepository extends JpaRepository<Reino, Integer>{
+public interface ReinoRepository extends CrudRepository<Reino, Integer>{
     
+   
 }
